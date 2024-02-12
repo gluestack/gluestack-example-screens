2e47390df2a76dad739fbd226553b88856eb25d3
Vaibhavi Kolloju <vaibhavikolloju@gmail.com>
2024-02-12 19:10:55 +0530

    feat: updated script to copy common components

--- example/storybook/generatesharable.js
+++ example/storybook/generatesharable.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 
+const commonComponentsPath = '../components/';
 const directoryPaths = [
   {
     directory: './src/example-screens/dashboard',
@@ -16,17 +17,53 @@ const directoryPaths = [
   },
 ];
 
+const fileRegex =
+  /import\s+((?:\*\s+as\s+[\w]+|{[^{}]*}|[\w\s,*]+))\s+(?:as\s+\w+\s+)?from\s+['"]([^'"]+)['"];/gm;
+const importsRegex =
+  /import\s+(?:(\{\s*([^}]*)\s*\})|\*\s+as\s+(\w+)|(\w+))\s+from\s+'([^']*)';/;
+const exportRegex =
+  /export\s+(?:(default\s+)?(?:\*\s+as\s+\w+|{[^{}]*}|\w+\s*)|(?:\*\s+from\s+)?)(?:['"]([^'"]+)['"])?;?/gm;
+
+const getImportMetaData = (importStatements) => {
+  const match = importStatements.match(importsRegex);
+  if (match) {
+    const [, named, namedGroup, namespace, defaultImport, source] = match;
+    return [named, namedGroup, namespace, defaultImport, source];
+  }
+  return undefined;
+};
+
+async function getComponentContent(file) {
+  const data = await fs.promises.readFile(file, 'utf8');
+  const imports = data.match(fileRegex);
+  const result = data.replace(fileRegex, '');
+  return { imports, result };
+}
+
 // function to remove all the imports from the file
 async function filterImports(file) {
-  const data = await fs.promises.readFile(file, 'utf8');
-  const imports = data.match(
-    /import\s+((?:\*\s+as\s+[\w]+|{[^{}]*}|[\w\s,*]+))\s+(?:as\s+\w+\s+)?from\s+['"]([^'"]+)['"];/gm
+  const { imports, result } = await getComponentContent(file);
+  if (!imports) return { result, imports };
+
+  //to get file content from imported components
+  const componentResult = await Promise.all(
+    imports?.map(async (statement) => {
+      const importMetaData = getImportMetaData(statement);
+      if (!importMetaData) return {};
+      const [named, namedGroup, namespace, defaultImport, source] =
+        importMetaData;
+      if (source?.startsWith(commonComponentsPath)) {
+        return getComponentContent(
+          `./src/example-screens/${source.replace('../', '')}.tsx`
+        );
+      }
+    })
   );
-  const result = data.replace(
-    /import\s+((?:\*\s+as\s+[\w]+|{[^{}]*}|[\w\s,*]+))\s+(?:as\s+\w+\s+)?from\s+['"]([^'"]+)['"];/gm,
-    ''
-  );
-  return { result, imports };
+  return {
+    result,
+    imports,
+    componentResult,
+  };
 }
 
 function mergeImports(importStatements) {
@@ -34,33 +71,28 @@ function mergeImports(importStatements) {
   const namespaceImports = new Set();
   const defaultImports = new Set();
 
-  // Regex to match different types of imports
-  const regex =
-    /import\s+(?:(\{\s*([^}]*)\s*\})|\*\s+as\s+(\w+)|(\w+))\s+from\s+'([^']*)';/;
-
   importStatements.forEach((statement) => {
-    const match = statement.match(regex);
-    if (match) {
-      const [, named, namedGroup, namespace, defaultImport, source] = match;
-      // Skip imports from the same directory
-      if (source.startsWith('./') && !source.startsWith('../')) {
-        return;
+    const importMetaData = getImportMetaData(statement);
+    if (!importMetaData) return;
+    const [named, namedGroup, namespace, defaultImport, source] =
+      importMetaData;
+    // Skip imports from the same directory
+    if (source.startsWith('./') && !source.startsWith('../')) {
+      return;
+    }
+    if (named) {
+      // Split named imports and remove whitespace
+      const names = namedGroup.split(',').map((name) => name.trim());
+      if (!namedImports[source]) {
+        namedImports[source] = new Set();
       }
-      if (named) {
-        // Split named imports and remove whitespace
-        const names = namedGroup.split(',').map((name) => name.trim());
-        if (!namedImports[source]) {
-          namedImports[source] = new Set();
-        }
-        names.forEach((name) => namedImports[source].add(name));
-      } else if (namespace) {
-        namespaceImports.add(`import * as ${namespace} from '${source}';`);
-      } else if (defaultImport) {
-        defaultImports.add(`import ${defaultImport} from '${source}';`);
-      }
+      names.forEach((name) => namedImports[source].add(name));
+    } else if (namespace) {
+      namespaceImports.add(`import * as ${namespace} from '${source}';`);
+    } else if (defaultImport) {
+      defaultImports.add(`import ${defaultImport} from '${source}';`);
     }
   });
-
   // Merge named imports
   const mergedNamedImports = Object.entries(namedImports).map(
     ([source, names]) => {
@@ -69,11 +101,9 @@ function mergeImports(importStatements) {
         .join(', ')} } from '${source}';`;
     }
   );
-
   // Convert Sets to Arrays
   const mergedNamespaceImports = Array.from(namespaceImports);
   const mergedDefaultImports = Array.from(defaultImports);
-
   // Combine all imports into a single array
   return [
     ...mergedDefaultImports,
@@ -85,7 +115,6 @@ function mergeImports(importStatements) {
 async function getCodeFromFiles(directoryPath, targetFile) {
   try {
     const files = await fs.promises.readdir(directoryPath);
-
     const allowedExtensions = ['.tsx', '.jsx', '.ts', '.js'];
     const filteredFiles = files.filter((file) => {
       const extension = path.extname(file);
@@ -99,15 +128,15 @@ async function getCodeFromFiles(directoryPath, targetFile) {
 
     let fileImports = [];
     let fileContent = '';
-    for (const file of filteredFiles) {
-      const filePath = path.join(directoryPath, file);
-      const { result, imports } = await filterImports(filePath);
-      if (imports) fileImports.push(...imports);
-      fileContent += result + '\n';
-    }
-
+    await Promise.all(
+      filteredFiles.map(async (file) => {
+        const filePath = path.join(directoryPath, file);
+        const { result, imports } = await filterImports(filePath);
+        if (imports) fileImports.push(...imports);
+        fileContent += result + '\n';
+      })
+    );
     const cleanedImports = mergeImports(fileImports);
-
     writeFileContent(fileContent, targetFile, cleanedImports);
   } catch (error) {
     console.error('Error:', error);
