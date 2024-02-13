11447d397f7f396c4bcb261ff4de858286f7622b
Vaibhavi Kolloju <vaibhavikolloju@gmail.com>
2024-02-13 16:39:20 +0530

    feat: added reusable component code

--- example/storybook/src/example-screens/login/login.tsx
+++ example/storybook/src/example-screens/login/login.tsx
@@ -12,12 +12,100 @@ import {
   ButtonIcon,
   Pressable,
   Image,
+  FormControl,
+  FormControlLabel,
+  FormControlLabelText,
+  InputSlot,
+  Input,
 } from '@gluestack-ui-new/themed';
-import CustomInput from '../components/CustomInput';
 import { GithubIcon } from './Icons';
 import { loginValidationSchema, loginValidationType } from './validations';
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
+import { useController } from 'react-hook-form';
+
+const CustomInput = ({
+  label,
+  icon,
+  children,
+  formControlProps,
+  inputProps,
+  validatorProps,
+}: {
+  label?: string;
+  icon?: any;
+  children: any;
+  formControlProps?: any;
+  inputProps?: any;
+  validatorProps?: any;
+}) => {
+  let childrenWithProps;
+  if (validatorProps) {
+    /* eslint-disable */
+    const {
+      field: { onChange, onBlur, value },
+      fieldState: { error, isTouched },
+    } = useController({
+      ...validatorProps,
+    });
+    childrenWithProps = React.cloneElement(children, {
+      onBlur: onBlur,
+      value: value,
+      onChangeText: (text: string) => {
+        onChange(text);
+        if (isTouched && validatorProps?.trigger) {
+          validatorProps.trigger(validatorProps?.name);
+        }
+      },
+    });
+    return (
+      <FormControl size="sm" {...formControlProps}>
+        {label && (
+          <FormControlLabel mb="$2">
+            <FormControlLabelText fontSize="$sm">{label}</FormControlLabelText>
+          </FormControlLabel>
+        )}
+        {icon ? (
+          <Input borderRadius="$lg" hardShadow="5" {...inputProps}>
+            <InputSlot pl="$3">{icon}</InputSlot>
+            {validatorProps ? childrenWithProps : children}
+          </Input>
+        ) : (
+          <Input {...inputProps}>
+            {validatorProps ? childrenWithProps : children}
+          </Input>
+        )}
+        {validatorProps?.trigger && (
+          <Text
+            color={error ? '$error600' : 'transparent'}
+            fontSize="$sm"
+            fontFamily="$body"
+            mt="$1"
+          >
+            {error ? error.message : ''}
+          </Text>
+        )}
+      </FormControl>
+    );
+  }
+  return (
+    <FormControl size="sm" {...formControlProps}>
+      {label && (
+        <FormControlLabel mb="$2">
+          <FormControlLabelText fontSize="$sm">{label}</FormControlLabelText>
+        </FormControlLabel>
+      )}
+      {icon ? (
+        <Input borderRadius="$lg" hardShadow="5" {...inputProps}>
+          <InputSlot pl="$3">{icon}</InputSlot>
+          {children}
+        </Input>
+      ) : (
+        <Input {...inputProps}>{children}</Input>
+      )}
+    </FormControl>
+  );
+};
 
 const Login = () => {
   const {
