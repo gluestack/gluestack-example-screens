9db4f99c652589d3d56e99ff4f351a88298dda1f
sra1kumar-NULL <sravansandeep163@gmail.com>
2024-02-19 18:32:02 +0530

    feat: added text fade in animation

---
+++ example/storybook/src/animations/text-fade-in/text-fade-in.stories.tsx
@@ -0,0 +1,9 @@
+import type { ComponentMeta } from '@storybook/react-native';
+import TextFadeIn from './text-fade-in';
+const TextFadeInMeta: ComponentMeta<typeof TextFadeIn> = {
+  title: 'animations/TextFadeIn',
+  component: TextFadeIn,
+};
+
+export default TextFadeInMeta;
+export { TextFadeIn };
